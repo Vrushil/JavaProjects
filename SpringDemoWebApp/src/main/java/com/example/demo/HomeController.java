a97e5010486e197984c393554831575e249dcb15
Vrushil Soni <vrushilsoni07@gmail.com>
2020-07-01 13:32:09 -0400

    Return display page

--- SpringDemoWebApp/src/main/java/com/example/demo/HomeController.java
+++ SpringDemoWebApp/src/main/java/com/example/demo/HomeController.java
@@ -2,15 +2,17 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
+	@ResponseBody  // This will print the return string on the web page . Spring boot does not support jsp by default. After adding dependencies in pom.xml for tomcat jasper. This wll help to return the page
 	public String home()
 	{
 	//	System.out.println("In home method");
- return "home.jsp";   // This will download  home.jsp loacted  in webapp folder. Searching the ifle in project  is autoconfigured
+ return "home";   // This will download  home.jsp loacted  in webapp folder. Searching the ifle in project  is autoconfigured
 	}
 
 }
--- SpringDemoWebApp/src/main/webapp/home.jsp
+++ SpringDemoWebApp/src/main/webapp/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+Hello to the home page 
 </body>
 </html>
