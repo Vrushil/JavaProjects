3a0ada5aeb74a23364df286fbc6e4bb3839201f5
Vrushil Soni <vrushilsoni07@gmail.com>
2020-07-02 18:43:41 -0400

    findByTech

--- SpringBootJpaDemo0/src/main/java/com/vrushil/demo/controller/AlienController.java
+++ SpringBootJpaDemo0/src/main/java/com/vrushil/demo/controller/AlienController.java
@@ -3,6 +3,7 @@ package com.vrushil.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.vrushil.demo.dao.AlienRepo;
 import com.vrushil.demo.model.Alien;
@@ -15,10 +16,21 @@ public class AlienController {
 	{
 		return "home";
 	}
-	@RequestMapping("/addAlien")
+	@RequestMapping("addAlien")
 	public String addAlien(Alien alien)
 	{
 		repo.save(alien);
 		return "home";
+	}
+	
+	@RequestMapping("getAlien")
+	public ModelAndView getAlien(int aid)
+	{
+		
+		ModelAndView mv= new ModelAndView("show");
+	Alien alien = repo.findById(aid).orElse(new Alien());
+	System.out.println(repo.findByTech("java"));
+	mv.addObject(alien);
+		return mv;
 	}
 }
