3b1cf916c2fa4638501f3e3476bad3a978528c4f
Vrushil Soni <vrushilsoni07@gmail.com>
2020-07-07 18:43:36 -0400

    Requires change in Application.yml

---
+++ SpringSecurityOAuth/src/main/java/com/vrushil/demo/SpringSecurityOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vrushil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuthApplication.class, args);
+	}
+
+}
